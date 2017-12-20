e18a2ce062b48e2ff918c2c1a9fb5f857b5d55b6
Jesse Baer <jesse.baer.hennig@gmail.com>
2017-12-20 11:27:06 -0500

    more clean + some script stuff

--- PaintCap/Assets/Scripts/Redtile.cs
+++ PaintCap/Assets/Scripts/Redtile.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using UnityEngine.Tilemaps;
 
-public class Redtile : MonoBehaviour {
+public class Redtile : Tile {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Tiles/RedTile")]
+
+    public static void CreateRedTile()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Save Red Tile", "New RedTile", "asset", "Save treeTile", "Assets");
+        if (path == "")
+        {
+            return;
+        }
+        AssetDatabase.CreateAsset(ScriptableObject.CreateInstance<Redtile>(), path);
+    }
+#endif
 }
